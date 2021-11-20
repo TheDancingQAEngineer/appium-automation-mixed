d4b6f5d51688e8660e1208e833e3b9e9f8e0f986
Yuriy Avsharov <thedancingqaengineer@gmail.com>
2021-11-20 20:27:45 +0300

    Начинаем урок 3-ей недели

---
+++ tests/junit/TestTextInElements.java
@@ -0,0 +1,68 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class TestTextInElements {
+
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("deviceName","andro80");
+        capabilities.setCapability("platformVersion","8.0");
+        capabilities.setCapability("automationName","Appium");
+        capabilities.setCapability("appPackage","org.wikipedia");
+        capabilities.setCapability("appActivity",".main.MainActivity");
+        capabilities.setCapability("app","/Users/user2/codebase/github/appium-automation-mixed/appium-automation-mixed/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown()
+    {
+        driver.quit();
+    }
+
+    @Test
+    public void testFindSearchFieldAndClick()
+    {
+        // GIVEN:
+        // - running emulator
+        // - running Appium Server
+
+        // WHEN: On app home screen
+
+        // THEN:
+        // - there's an element with text 'Search Wikipedia';
+        // - the element is clickable
+        WebElement element_to_init_search = driver.findElementByXPath("//*[contains(@text, 'Search Wikipedia')]");
+        element_to_init_search.click();
+    }
+
+    @Test
+    public void testSendTextToSearchField()
+    {
+        // GIVEN:
+        // - running emulator
+        // - running Appium Server
+
+        // WHEN: On search screen
+        WebElement element_to_init_search = driver.findElementByXPath("//*[contains(@text, 'Search Wikipedia')]");
+        element_to_init_search.click();
+
+        // THEN: Search field accepts text
+        WebElement element_to_type_search_query = driver.findElementByXPath("//*[contains(@text, 'Search…')]");
+        element_to_type_search_query.sendKeys("Yo La Tengo Discography");
+    }
+}
