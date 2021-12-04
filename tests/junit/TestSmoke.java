ece1ca0fce8b07d19e42976b4f38a853820fe625
Yuriy Avsharov <thedancingqaengineer@gmail.com>
2021-12-04 19:34:05 +0300

    Урок 5. Добавлен класс CoreTestCase

---
+++ tests/junit/TestSmoke.java
@@ -0,0 +1,11 @@
+import lib.CoreTestCase;
+import org.junit.*;
+
+public class TestSmoke extends CoreTestCase {
+
+    @Test
+    public void testSmoke()
+    {
+        System.out.println("I am testSmoke(). I ran!");
+    }
+}
