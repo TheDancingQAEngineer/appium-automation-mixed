f58607ea5270788dfb9f419bcc4b64b5f6d2176d
Yuriy Avsharov <thedancingqaengineer@gmail.com>
2021-12-06 22:32:10 +0300

    Урок 5, Ex9, попытка 1.
    Только пустой тест и фреймворк по состоянию на конец части 6 урока 5.

---
+++ tests/junit/TestEx9.java
@@ -0,0 +1,13 @@
+import lib.CoreTestCase;
+import org.junit.Test;
+
+public class TestEx9 extends CoreTestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Test
+    public void testArticleStrictMatching() { }
+}
