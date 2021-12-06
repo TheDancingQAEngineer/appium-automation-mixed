f9e3b389a865f5a53e3dcdf88230ccb323e760d4
Yuriy Avsharov <thedancingqaengineer@gmail.com>
2021-12-06 21:57:24 +0300

    Урок 5, части 1-6 и ДЗ Ex8

--- tests/junit/lib/CoreTestCase.java
+++ tests/junit/lib/CoreTestCase.java
@@ -3,6 +3,9 @@ package lib;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import junit.framework.TestCase;
+import lib.ui.ArticlePageObject;
+import lib.ui.MainPageObject;
+import lib.ui.SearchPageObject;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
@@ -10,10 +13,11 @@ import java.net.URL;
 public class CoreTestCase extends TestCase {
 
     protected AppiumDriver driver;
+
     private static final String AppiumURL = "http://127.0.0.1:4723/wd/hub";
 
     @Override
-    public void setUp() throws Exception
+    protected void setUp() throws Exception
     {
         super.setUp();
 
@@ -22,11 +26,11 @@ public class CoreTestCase extends TestCase {
         capabilities.setCapability("platformName","Android");
         capabilities.setCapability("deviceName","andro80");
         capabilities.setCapability("platformVersion","8.0");
-        capabilities.setCapability("automationName","Appium");
+        capabilities.setCapability("automationName","UiAutomator2");
         capabilities.setCapability("appPackage","org.wikipedia");
         capabilities.setCapability("appActivity",".main.MainActivity");
         capabilities.setCapability("app","***REMOVED***/codebase/github/appium-automation-mixed/appium-automation-mixed/apks/org.wikipedia.apk");
-        capabilities.setCapability("adbExecTimeout", 40000);
+        capabilities.setCapability("appWaitDuration", 40000);
         capabilities.setCapability("orientation", "PORTRAIT");
 
         driver = new AndroidDriver(new URL(AppiumURL), capabilities);
