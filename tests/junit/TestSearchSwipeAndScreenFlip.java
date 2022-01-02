1003ecc1890724b7adcff032aeb40cecb41e0220
Yuriy Avsharov <thedancingqaengineer@gmail.com>
2022-01-03 02:41:57 +0300

    Урок 6. Рефакторинг окончен.
    Тесты на своих платформах проходят. Тип драйвера и caps переключаются по переменной окружения, как в уроке

--- tests/junit/TestSearchSwipeAndScreenFlip.java
+++ tests/junit/TestSearchSwipeAndScreenFlip.java
@@ -110,23 +110,9 @@ public class TestSearchSwipeAndScreenFlip extends CoreTestCase {
         SearchPageObject.initSearchInput();
 
         // Send query with at least one likely match
-        UiHelpers.waitForElementVisibleAndSendKeys(driver,
-                By.xpath("//*[contains(@text, 'Search…')]"),
-                search_line,
-                String.format("Cannot type '%s' to search.", search_line),
-                10);
+        SearchPageObject.typeSearchLine(search_line);
 
-
-        String search_result_locator = "//*[@resource-id='org.wikipedia:id/search_results_container']" +
-                "//*[@resource-id='org.wikipedia:id/page_list_item_container']";
-
-        UiHelpers.waitForElementVisible(driver,
-                By.xpath(search_result_locator),
-                String.format("Failed to locate element by xpath: \n%s.", search_result_locator),
-                10);
-
-        int number_of_search_elements = UiHelpers.getNumberOfElements(driver,
-                By.xpath(search_result_locator));
+        int number_of_search_elements = SearchPageObject.getNumberOfSearchResults();
 
         // Check that search results has more than 0 items, if true, pass
         Assert.assertTrue("Too few results", number_of_search_elements > 0);
@@ -137,34 +123,19 @@ public class TestSearchSwipeAndScreenFlip extends CoreTestCase {
     public void testAmountOfEmptySearch()
     {
         String search_line = "VCXZasdfrewq";
-        // String search_line = "zxcvfdsaqwer";
-
-        // String no_results_label_locator = "//*[@resource-id='org.wikipedia:id/search_empty_view']";
-        String no_results_label_locator = "//*[@text='No results found']";
-        String search_result_locator = "//*[@resource-id='org.wikipedia:id/search_results_container']" +
-                "//*[@resource-id='org.wikipedia:id/page_list_item_container']";
 
         // Open app
         // Tap 'Search'
         SearchPageObject.initSearchInput();
 
         // Send random query with unlikely matches
-        UiHelpers.waitForElementVisibleAndSendKeys(driver,
-                By.xpath("//*[contains(@text, 'Search…')]"),
-                search_line,
-                String.format("Cannot type '%s' to search.", search_line),
-                10);
+        SearchPageObject.typeSearchLine(search_line);
 
         // Check that search results has 0 items, if false, throw exception
-        UiHelpers.assertZeroElementsVisible(driver,
-                By.xpath(search_result_locator),
-                String.format("Found results by query: '%s'", search_line));
+        SearchPageObject.assertZeroSearchResults(search_line);
 
         // Check that "No results found" label is visible
-        UiHelpers.waitForElementVisible(driver,
-                By.xpath(no_results_label_locator),
-                String.format("Cannot locate item by xpath: \n%s", no_results_label_locator),
-                10);
+        SearchPageObject.waitForNoResultsLabel();
     }
 
     /* Part 7. Rotation */
@@ -181,39 +152,20 @@ public class TestSearchSwipeAndScreenFlip extends CoreTestCase {
 
         // Send search query
         String search_query = "Java";
-        UiHelpers.waitForElementVisibleAndSendKeys(driver,
-                By.xpath("//*[contains(@text, 'Search…')]"),
-                search_query,
-                "Cannot locate search input field.",
-                5);
+        String expected_substring = "Object-oriented programming language";
+        SearchPageObject.typeSearchLine(search_query);
 
         // Wait for results
-        String xpath_to_search =
-                "//*[@resource-id='org.wikipedia:id/page_list_item_container']"
-                        + "//*[@text='Object-oriented programming language']";
-
-        // Open article
-        UiHelpers.waitForElementVisibleAndClick(driver,
-                By.xpath(xpath_to_search),
-                String.format("Cannot locate element by xpath: \n%s", xpath_to_search),
-                15);
+        SearchPageObject.clickOnArticleWithSubstring(expected_substring);
 
         // Get title
-        String title_before_rotation = MainPageObject.waitForElementVisibleAndGetAttribute(
-                "org.wikipedia:id/view_page_title_text",
-                "text",
-                "Cannot find article title.",
-                15);
+        String title_before_rotation = ArticlePageObject.getArticleTitle();
 
         // Rotate device
         driver.rotate(ScreenOrientation.LANDSCAPE);
 
         // Assert article title is still there
-        String title_after_rotation = MainPageObject.waitForElementVisibleAndGetAttribute(
-                "org.wikipedia:id/view_page_title_text",
-                "text",
-                "Cannot find article title.",
-                15);
+        String title_after_rotation = ArticlePageObject.getArticleTitle();
 
         Assert.assertEquals(
                 "Titles before and after rotation don't match.",
@@ -225,11 +177,7 @@ public class TestSearchSwipeAndScreenFlip extends CoreTestCase {
         driver.rotate(ScreenOrientation.LANDSCAPE);
 
         // Assert article title is still the same
-        String title_after_second_rotation = MainPageObject.waitForElementVisibleAndGetAttribute(
-                "org.wikipedia:id/view_page_title_text",
-                "text",
-                "Cannot find article title.",
-                15);
+        String title_after_second_rotation = ArticlePageObject.getArticleTitle();
 
         Assert.assertEquals(
                 "Titles before and after two rotations don't match.",
@@ -255,30 +203,16 @@ public class TestSearchSwipeAndScreenFlip extends CoreTestCase {
         String search_query = "Java";
         String article_title = "Java (programming language)";
 
-        UiHelpers.waitForElementVisibleAndSendKeys(driver,
-                By.xpath("//*[contains(@text, 'Search…')]"),
-                search_query,
-                "Cannot locate search input field.",
-                5);
+        SearchPageObject.typeSearchLine(search_query);
 
         // Check that 'Java (programming language)' is found
-        String xpath_to_search =
-                "//*[@resource-id='org.wikipedia:id/page_list_item_title']"
-                        + String.format("[@text='%s']", article_title);
-
-        UiHelpers.waitForElementVisible(driver,
-                By.xpath(xpath_to_search),
-                String.format("Cannot find '%s' in search results.", article_title),
-                10);
+        SearchPageObject.waitForSearchResult(article_title);
 
         // Send app to background by clicking 'O'
         // Launch again
         driver.runAppInBackground(java.time.Duration.ofSeconds(2));
 
         // Check that our search result is still on the screen
-        UiHelpers.waitForElementVisible(driver,
-                By.xpath(xpath_to_search),
-                String.format("Cannot find '%s' in search results.", article_title),
-                10);
+        SearchPageObject.waitForSearchResult(article_title);
     }
 }
