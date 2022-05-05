b2412771152af796da12d0c15a6da62d023d7fb0
Yuriy Avsharov <thedancingqaengineer@gmail.com>
2022-05-05 18:14:12 +0300

    Refactoring SearchPageObject to support Firefox

---
+++ src/test/java/lib/ui/mobileweb/firefox/MWFirefoxSearchPageObject.java
@@ -0,0 +1,35 @@
+package lib.ui.mobileweb.firefox;
+
+import lib.ui.SearchPageObject;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class MWFirefoxSearchPageObject extends SearchPageObject {
+
+    static {
+        NO_RESULTS_LABEL_XPATH = "css:div.caption>p.without-results"; // OK
+        SEARCH_INIT_ELEMENT_XPATH = "css:button#searchIcon"; // OK
+        SEARCH_TEXT_FIELD_LOCATOR = "css:form>input[type='search']"; // OK
+        SEARCH_CANCEL_BUTTON_LOCATOR = "css:div>button.cancel"; // OK
+        SEARCH_RESULT_LOCATOR = "css:ul.page-list>li.page-summary"; // OK
+    }
+
+    /** STRING TEMPLATES BEGIN **/
+
+    static {
+        // Should work if either title or description contains {SUBSTRING}
+        SEARCH_RESULT_BY_SUBSTRING_XPATH_TPL =
+                "xpath://li[contains(@class, 'page-summary')][contains(@title, '{SUBSTRING}') or .//*[contains(@class, 'description')][contains(text(), '{SUBSTRING}')]]";
+
+        // Should work if title contains {TITLE} AND description contains {DESCRIPTION}
+        SEARCH_ITEM_TITLE_AND_DESCRIPTION_TPL =
+                "xpath://li[contains(@class, 'page-summary')][@title='{TITLE}' and .//*[contains(@class, 'description')][contains(text(), '{DESCRIPTION}')]]";
+    }
+
+    /** STRING TEMPLATES END **/
+
+    /** CONSTRUCTOR BEGINS **/
+        public MWFirefoxSearchPageObject(RemoteWebDriver driver) {
+        super(driver);
+    }
+    /** CONSTRUCTOR ENDS **/
+}
